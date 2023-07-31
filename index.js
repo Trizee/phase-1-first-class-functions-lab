ac36ba14654364dc0667b501de1bcae4ad692b0e
Tri Hoang <trihoang09@gmail.com>
2023-07-31 07:41:21 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(){
+    return function(fare){
+       return (fare)*5
+    }
+}
+
+function fareDoubler(fare){
+    return fare * 2
+}
+
+function fareTripler(fare){
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers,selection){
+     if(selection === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers)
+     } else if (selection === returnLastTwoDrivers){
+        return returnLastTwoDrivers(drivers)
+     }
+}
